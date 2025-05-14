34f2371f605bbe362fb3800175af133adb9f8bad
Truc1p <trucph6789@icloud.com>
2025-05-14 13:59:23 +0700

    commit

--- script.js
+++ script.js
@@ -218,8 +218,7 @@ let recommendation = null; // Add a global variable for recommendation
 const API_URL = "http://127.0.0.1:8000/nlp/process_text_and_get_nutrition/";
 const GEMINI_API_URL = "http://127.0.0.1:5000/ai/chat/"; 
 // const QWEN_API_URL = "http://127.0.0.1:1234/v1/chat/completions";
-
-const QWEN_API_URL = "http://localhost:8000/chat";
+// const QWEN_API_URL = "http://localhost:8000/chat";
 
 // --- Event Listeners ---
 foodInput.addEventListener('input', handleInputChange);
