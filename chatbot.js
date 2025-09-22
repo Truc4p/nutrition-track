8fc134e013b5cb417e72c1d4d6ca2bc1435ee575
Truc1p <truc4pham@gmail.com>
2025-09-22 18:17:54 +0700

    commit

--- chatbot.js
+++ chatbot.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // // Replace with your Gemini API key
 const GEMINI_KEY = 'AIzaSyAZbp4SEeaAq8ioyvuWNF7kcwalhNA8h8I';
